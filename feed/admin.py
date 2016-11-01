8951059d5ff50bb4a4b21337507db747900165bf
Paweł Koniarski <me@pkoniarski.com>
2016-11-01 13:02:24 +0100

    Add Person model to admin

--- feed/admin.py
+++ feed/admin.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import Tweet
+from .models import Tweet, Person
 
 admin.site.register(Tweet)
+admin.site.register(Person, UserAdmin)
