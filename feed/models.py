0475baced1a21d391ca199fe00a6e20d17963f3d
Paweł Koniarski <me@pkoniarski.com>
2016-11-01 11:34:07 +0100

    Add Tweet model

--- feed/models.py
+++ feed/models.py
@@ -8,3 +8,8 @@ from django.contrib.auth.models import AbstractUser
 class Person(AbstractUser):
     pass
 
+
+class Tweet(models.Model):
+    author = models.ForeignKey(Person, on_delete=models.CASCADE)
+    body = models.CharField(max_length=140)
+    timestamp = models.DateTimeField('date published')
