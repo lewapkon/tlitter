6dee0cd7249afbcff4f9a998b3b8771f0efbcd6f
Paweł Koniarski <me@pkoniarski.com>
2016-11-01 13:18:51 +0100

    Add Tweet ordering

--- feed/models.py
+++ feed/models.py
@@ -8,6 +8,9 @@ from django.contrib.auth.models import AbstractUser
 
 @python_2_unicode_compatible
 class Person(AbstractUser):
+    class Meta:
+        verbose_name_plural = 'people'
+
     def __str__(self):
         return self.get_full_name()
 
@@ -17,6 +20,9 @@ class Tweet(models.Model):
     author = models.ForeignKey(Person, on_delete=models.CASCADE)
     body = models.CharField(max_length=140)
     timestamp = models.DateTimeField('date published')
+
+    class Meta:
+        ordering = ['-timestamp']
 
     def __str__(self):
         return '{}: {}'.format(self.author, self.body)
